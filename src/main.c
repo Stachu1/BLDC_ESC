7d74303e0b17a1c69deb0ae0a6071ec37d1a5832
Stachu1 <stach.basiuk@gmail.com>
2024-10-01 16:12:47 +0200

    3-half bridge switching

---
+++ src/main.c
@@ -0,0 +1,86 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+
+// A_HIGH: PD2
+// B_HIGH: PD4
+// C_HIGH: PD7
+
+// A_LOW: PD3
+// B_LOW: PD5
+// C_LOW: PD6
+
+#define A_H 2
+#define B_H 4
+#define C_H 7
+
+#define A_L 3
+#define B_L 5
+#define C_L 6
+
+
+
+void step_1() {
+  DDRD = (1 << B_H) | (1 << A_L);
+  PORTD = (1 << A_L);
+}
+
+void step_2() {
+  DDRD = (1 << B_H) | (1 << C_L);
+  PORTD = (1 << C_L);
+}
+
+void step_3() {
+  DDRD = (1 << A_H) | (1 << C_L);
+  PORTD = (1 << C_L);
+}
+
+void step_4() {
+  DDRD = (1 << A_H) | (1 << B_L);
+  PORTD = (1 << B_L);
+}
+
+void step_5() {
+  DDRD = (1 << C_H) | (1 << B_L);
+  PORTD = (1 << B_L);
+}
+
+void step_6() {
+  DDRD = (1 << C_H) | (1 << A_L);
+  PORTD = (1 << A_L);
+}
+
+
+
+int main(void){               // Call the USART initialization code
+
+  while(1)  {
+    static uint8_t step = 1;
+
+    switch (step) {
+      case 1:
+        step_1();
+        break;
+      case 2:
+        step_2();
+        break;
+      case 3:
+        step_3();
+        break;
+      case 4:
+        step_4();
+        break;
+      case 5:
+        step_5();
+        break;
+      case 6:
+        step_6();
+        break;
+    }
+    step++;
+    if (step == 7) step = 1;
+    _delay_ms(1000);
+  }
+  return 0;
+}
