d3e36c81e8180699a0c2ae075f2bfdcc09ddadc3
Stachu1 <stach.basiuk@gmail.com>
2024-10-04 11:18:19 +0200

    The motor does spin but needs more power to be tested

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include <stdio.h>
 
 
@@ -19,6 +20,12 @@
 #define B_L 5
 #define C_L 6
 
+
+
+volatile uint32_t millis = 0;
+volatile uint8_t step = 1;
+volatile uint8_t speed_div = 32;
+uint8_t target_speed_div = 4;
 
 
 void step_1() {
@@ -52,13 +59,34 @@ void step_6() {
 }
 
 
+ISR (TIMER0_OVF_vect) {
+  millis++;
+  if (millis % speed_div == 0) {
+    step++;
+    if (step == 7) step = 1;
+  }
+}
 
-int main(void){               // Call the USART initialization code
+
+int main(void) {
+  cli();
+  TCCR0B |= 0b00000011;
+  TIMSK0 |= 0b00000001;
+  sei();
+
+  uint8_t last_step;
+  uint32_t last_millis = 0;
 
   while(1)  {
-    static uint8_t step = 1;
+    if (millis - last_millis > 100) {
+      last_millis = millis;
+      if (speed_div != target_speed_div) speed_div--;
+    }
 
-    switch (step) {
+
+    if (last_step != step) {
+      last_step = step;
+      switch (step) {
       case 1:
         step_1();
         break;
@@ -78,9 +106,7 @@ int main(void){               // Call the USART initialization code
         step_6();
         break;
     }
-    step++;
-    if (step == 7) step = 1;
-    _delay_ms(1000);
+    }
   }
   return 0;
 }
