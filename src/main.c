650c3064f1a93ade29e5d680b1c99106ba4387ac
Stachu1 <stach.basiuk@gmail.com>
2024-11-21 00:51:01 +0100

    Comments and descriptions

--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@
 #include <util/delay.h>
 #include "usart.h"
 
-#define MIN_TARGET 40
-#define START_DUTY_CYCLE 100
-#define DEBOUNCE 10
-#define MAX_DELTA_RPS 100
+#define MIN_TARGET 40                     // Minimum target speed in RPS
+#define START_DUTY_CYCLE 100              // Duty cycle for open loop start
+#define DEBOUNCE 10                       // BEMF debounce count - default is 10 consecutive readings
+#define MAX_DELTA_RPS 100                 // Maximum acceptable delta RPS before restarting the motor
 
 // Define 3-half bridge pins
 #define AH PD5
@@ -19,8 +19,10 @@
 #define BL PB2
 #define CL PB1
 
-#define Kp 0.2f   // Proportional gain as a float macro
-#define Kd 0.5f   // Derivative gain as a float macro
+// PID controller constants
+#define Kp 0.2f                           // Proportional gain
+#define Kd 0.5f                           // Derivative gain
+
 
 volatile uint8_t step = 0;
 volatile uint16_t steps_count = 0;
@@ -69,15 +71,15 @@ int main(void) {
   TCCR2B = (1 << CS20);
 
   // Timer0: 1ms interrupt
-  TCCR0A = (1 << WGM01);                    // Set CTC mode
-  TCCR0B = (1 << CS01) | (1 << CS00);       // Prescaler 64
-  OCR0A = 249;                              // Compare match for 1ms
-  TIMSK0 |= (1 << OCIE0A);                  // Enable Timer0 compare interrupt
+  TCCR0A = (1 << WGM01);                  // Set CTC mode
+  TCCR0B = (1 << CS01) | (1 << CS00);     // Prescaler 64
+  OCR0A = 249;                            // Compare match for 1ms
+  TIMSK0 |= (1 << OCIE0A);                // Enable Timer0 compare interrupt
   
   // Analog comparator setting
-  ACSR &= ~(1 << ACIE);                     // Disable the Analog Comparator interrupt
-  ACSR |= (1 << ACI);                       // Clear the Analog Comparator interrupt flag
-  sei();                                    // Enable global interrupts
+  ACSR &= ~(1 << ACIE);                   // Disable the Analog Comparator interrupt
+  ACSR |= (1 << ACI);                     // Clear the Analog Comparator interrupt flag
+  sei();                                  // Enable global interrupts
 
   while(1) {
 
@@ -100,8 +102,8 @@ int main(void) {
     // Update RPS & delta_rps every 100ms
     if (millis_count % 100 == 0) {
       delta_rps = rps;
-      rps = (steps_count / 4.2);      // Calculate RPS
-      delta_rps = rps - delta_rps;    // Calculate delta RPS
+      rps = (steps_count / 4.2);          // Calculate RPS
+      delta_rps = rps - delta_rps;        // Calculate delta RPS
       steps_count = 0;
 
       // Update duty cycle to reach the target speed
